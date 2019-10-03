22873720886177575104628998d01b4c16627653
nikitusik <nikitacpmi@gmail.com>
2019-10-03 21:21:11 +0400

    feat: add solution function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.center = Math.round((this.min+this.max)/2);
     }
 
     lower() {
-
+        this.max = this.center;
     }
 
     greater() {
-
+        this.min = this.center;
     }
 }
 
